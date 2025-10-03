e476864abdce137961d1f3a4abf609ed6d3cfc9e
LucasRafa22 <lucassolimene22@gmail.com>
2025-10-03 11:44:52 -0300

    feat: rota de site criada

--- access-control-cp/src/main.tsx
+++ access-control-cp/src/main.tsx
@@ -6,11 +6,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Error from './routes/Error/index.tsx';
 import Login from './routes/Login/index.tsx';
 import Cadastro from './routes/Cadastro/index.tsx';
+import Site from './routes/Site/index.tsx';
 
 const router = createBrowserRouter([
   {path:'/', element:<App/>, errorElement: <Error/>, children:[
     {path:'/', element:<Login/>},
-    {path:'/cadastro', element: <Cadastro/>}
+    {path:'/cadastro', element: <Cadastro/>},
+    {path:'/site', element: <Site/>}
   
   ]}
 ])
---
+++ access-control-cp/src/routes/Site/index.tsx
@@ -0,0 +1,7 @@
+export default function Site(){
+    return(
+        <main>
+            <h1>Bem vindo ao nosso site!</h1>
+        </main>
+    )
+}
