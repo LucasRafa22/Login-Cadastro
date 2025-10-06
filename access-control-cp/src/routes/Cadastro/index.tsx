e1f4c25ff3fdb8c143b5bfee0fb797862d6084ef
samyr <costosinho50@gmail.com>
2025-10-06 00:23:30 -0300

    tailwind no cadastro

--- access-control-cp/src/routes/Cadastro/index.tsx
+++ access-control-cp/src/routes/Cadastro/index.tsx
@@ -59,55 +59,57 @@ export default function Cadastro() {
     };
 
     return (
-        <main>
-            <h1>Cadastro</h1>
-            <div>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <h2>Formulário de Cadastro</h2>
+        <main className="pagina_conteudo">
+            <div className="form_card">
+                <h1 className="titulo_principal">Cadastro</h1>
+                <h2 className="subtitulo">Formulário de Cadastro</h2>
 
-                    <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="login_requisitos">
                         <label htmlFor="nome">Nome</label>
                         <input
                             id="nome"
                             type="text"
+                            className="texto_botao"
                             {...register("nome", {required:'O nome é obrigatorio', 
                                 minLength: { value: 3, message: "O nome deve ter no mínimo 3 caracteres" },
                                 maxLength: { value: 50, message: "O nome deve ter no maximo 50 caracteres" }})}
                         />
-                        {errors.nome && <span>{errors.nome.message}</span>}
+                        {errors.nome && <span className="errorMsg">{errors.nome.message}</span>}
                     </div>
 
-                    <div>
+                    <div className="login_requisitos">
                         <label htmlFor="nomeUsuario">Nome de Usuário</label>
                         <input
                             id="nomeUsuario"
                             type="text"
-                            
+                            className="texto_botao"
                             {...register("nomeUsuario", {required:'O nome de usuário é obrigatorio', 
                                 minLength: { value: 3, message: "O nome de usuário deve ter no mínimo 3 caracteres" },
                                 maxLength: { value: 200, message: "O nome de usuário deve ter no maximo 200 caracteres" },
                                 validate: async (value) => await validateUnique('nomeUsuario', value)
                             })}
                         />
-                        {errors.nomeUsuario && <span>{errors.nomeUsuario.message}</span>}
+                        {errors.nomeUsuario && <span className="errorMsg">{errors.nomeUsuario.message}</span>}
                     </div>
 
-                    <div>
+                    <div className="login_requisitos">
                         <label htmlFor="email">Email</label>
                         <input
                             id="email"
                             type="email"
+                            className="texto_botao"
                             {...register("email", { required: "O email é obrigatório", 
                                 minLength: { value: 3, message: "O email deve ter no mínimo 3 caracteres" },
                                 maxLength: { value: 200, message: "O email deve ter no maximo 200 caracteres" },
                                 validate: async (value) => await validateUnique('email', value)
                             })}
                         />
-                        {errors.email && <span>{errors.email.message}</span>}
+                        {errors.email && <span className="errorMsg">{errors.email.message}</span>}
                     </div>
 
                     <div>
-                        <button type="submit">
+                        <button type="submit" className="botao_confirmar">
                             Cadastrar
                         </button>
                     </div>
